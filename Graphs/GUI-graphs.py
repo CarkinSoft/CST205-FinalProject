583b371d284b1fbedff4a49bc0937320a666b4c3
Shea Gallagher <sheafgallagher@gmail.com>
2025-04-22 16:19:24 -0700

    Improved the graph window GUI, it can now use the graph functions from graphs.py to make a graph image. Still working on displaying the graph in the window

--- Graphs/GUI-graphs.py
+++ Graphs/GUI-graphs.py
@@ -1,68 +1,88 @@
 import sys
-from PySide6.QtWidgets import (QApplication, QWidget, QVBoxLayout,
-                                QLabel, QLineEdit, QComboBox,
-                                 QPushButton)
+import os
+import graphs # graphs.py
+from PySide6.QtWidgets import (QApplication, QWidget, QVBoxLayout, QLabel, 
+							   QLineEdit, QComboBox, QPushButton)
 from PySide6.QtCore import Qt
+from PySide6.QtGui import QPixmap
 from __feature__ import snake_case, true_property
 
 # Set up app
 my_app = QApplication([])
 
 class GraphWindow(QWidget):
-  def __init__(self):
-      super().__init__()
+	def __init__(self):
+		super().__init__()
 
-      # Main feature layout
-      main_layout = QVBoxLayout()
-      self.title = QLabel("Audio Graphs")
+		# Main feature layout
+		main_layout = QVBoxLayout()
+		self.resize(500, 300)
+		self.title = QLabel("Audio Graphs")
 
-      # File input
-      self.filename = QLineEdit("Enter file name with extension (.mp3, .wav)")
-      main_layout.add_widget(self.filename)
+		# File input
+		self.filename = QLineEdit("Enter file name with extension (.mp3, .wav)")
+		main_layout.add_widget(self.filename)
 
-      # Graph selection
-      self.cbox = QComboBox()
-      self.desc_label = QLabel("Description: ")
-      # Default prompt
-      self.cbox.add_item("Pick a graph")
-      self.cbox.set_item_data(0, 0, role=Qt.UserRole - 1)
-      # Graph descriptions
-      self.graph_desc = {"MFCC" : "Mel-frequency cepstral coefficients, sometimes refered to as audio finger prints are used in music and speech recognition",
-                    "Spectrogram" : "Visualizes how the frequency of sound change over time.",
-                    "Beat Markers" : "Displays detected beat positions in the audio signal.",
-                    "Waveform" : "A sound wave showing the amplitude (loudness) of the audio signal over time.",
-                    "Mel" : "A spectrogram scaled to better represent how humans percive sound amplitude",
-                    }
-      self.cbox.add_items(self.graph_desc.keys())
-      self.cbox.currentTextChanged.connect(self.update_desc)
-      main_layout.add_widget(self.cbox)
-      main_layout.add_widget(self.desc_label)
+		# Graph selection
+		self.cbox = QComboBox()
+		self.desc_label = QLabel("Description: ")
+		# Default prompt
+		self.cbox.add_item("Pick a graph")
+		self.cbox.set_item_data(0, 0, role=Qt.UserRole - 1)
+		# Graph descriptions
+		self.graph_desc = {"MFCC" : "Mel-frequency cepstral coefficients, sometimes refered to as audio finger prints are used in music and speech recognition",
+						"Spectrogram" : "Visualizes how the frequency of sound change over time.",
+						"Beat Markers" : "Displays detected beat positions in the audio signal.",
+						"Waveform" : "A sound wave showing the amplitude (loudness) of the audio signal over time.",
+						"Mel" : "A spectrogram scaled to better represent how humans percive sound amplitude",
+						}
+		self.cbox.add_items(self.graph_desc.keys())
+		self.cbox.currentTextChanged.connect(self.update_desc)
+		main_layout.add_widget(self.cbox)
+		main_layout.add_widget(self.desc_label)
 
-      # Create graph button
-      btn = QPushButton("Make Graph!")
-      # Function call on button click, not implemented yet
-      # btn.clicked.connect(self.make_graph)
-      main_layout.add_widget(btn)
+		# Create graph button
+		btn = QPushButton("Make Graph!")
+		# Function call on button click
+		btn.clicked.connect(self.make_graph)
+		main_layout.add_widget(btn)
 
-      # Display graph
-      self.graph_image = QLabel()
-      main_layout.add_widget(self.graph_image)
+		# Display graph
+		self.graph_image = QLabel()
+		main_layout.add_widget(self.graph_image)
 
-      # Display window
-      self.set_layout(main_layout)
-      self.show()
+		# Display window
+		self.set_layout(main_layout)
+		self.show()
 
-  # when a graph is selected from the combo box, give a description of what its displaying
-  def update_desc(self, graph_name):
-    if graph_name in self.graph_desc:
-      graph_desc = self.graph_desc[graph_name]
-      self.desc_label.text = f"Description: {graph_desc}"
-    else:
-       self.desc_label.text = f"Description:"
+  	# when a graph is selected from the combo box, give a description of what its displaying
+	def update_desc(self, graph_type):
+		if graph_type in self.graph_desc:
+			graph_desc = self.graph_desc[graph_type]
+			self.desc_label.text = f"Description: {graph_desc}"
+		else:
+			self.desc_label.text = f"Description:"
 
-  # def make_graph():
-  #    return
-
+	# generates graph	
+	def make_graph(self):
+		filename = self.filename.text
+		graph_type = self.cbox.current_text
+		# Create path to audio file folder
+		file_path = os.path.join("TestAudioFiles", filename)
+		if not os.path.exists(file_path):
+			print("File not found:", file_path)
+			return
+		
+		# Generate graph using graphs.py
+		# TODO Graph img is created but not displayed
+		try:
+			output_img = graphs.make_graph(file_path, graph_type)
+			# Create Qt image
+			graph_pixmap = QPixmap(output_img)
+			self.graph_image.set_pixmap(graph_pixmap)
+		except Exception as e:
+			self.desc_label.text = f"Error generating graph"
+    
 
 my_win = GraphWindow()
 
--- Graphs/graphs.py
+++ Graphs/graphs.py
@@ -10,17 +10,17 @@ plt.rcParams['text.color'] = '#FFFFFF'
 plt.rcParams['axes.labelcolor'] = '#FFFFFF'
 plt.rcParams['xtick.color'] = '#FFFFFF'
 plt.rcParams['ytick.color'] = '#FFFFFF'
-plt.figure(figsize=(10, 4), facecolor='#1F1B24')
 
-def dis_mfccs(y, sr):
+def dis_mfccs(y, sr, save_path):
     # Extract MFCCs
     mfccs = librosa.feature.mfcc(y=y, sr=sr)
     librosa.display.specshow(mfccs, x_axis='time', cmap='magma') # cmap to change color
     plt.colorbar(format='%+2.0f dB')
     plt.title('MFCCs')
-    plt.show()
+    plt.savefig(save_path, facecolor='#1F1B24')
+    plt.close()
 
-def dis_spectrogram(y, sr):
+def dis_spectrogram(y, sr, save_path):
     # Compute spectrogram
     S = librosa.stft(y)
     S_db = librosa.amplitude_to_db(np.abs(S), ref=np.max)
@@ -32,10 +32,11 @@ def dis_spectrogram(y, sr):
     plt.title('Spectrogram (Time-Frequency Energy)', fontsize=16, fontweight='bold')
     plt.colorbar(format='%+2.0f dB', label='Amplitude (dB)')
     plt.tight_layout()
-    plt.show()
+    plt.savefig(save_path, facecolor='#1F1B24')
+    plt.close()
 
 
-def dis_beatmarkers(y, sr):
+def dis_beatmarkers(y, sr, save_path):
     # Beat tracking
     tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
     beat_times = librosa.frames_to_time(beat_frames, sr=sr)
@@ -49,10 +50,11 @@ def dis_beatmarkers(y, sr):
     plt.xlabel('Time (seconds)')
     plt.ylabel('Amplitude')
     plt.tight_layout()
-    plt.show()
+    plt.savefig(save_path, facecolor='#1F1B24')
+    plt.close()
 
 
-def dis_waveform(y, sr):
+def dis_waveform(y, sr, save_path):
     # Plot the waveform
     librosa.display.waveshow(y, sr=sr, color='cyan', alpha=0.8)
 
@@ -61,10 +63,11 @@ def dis_waveform(y, sr):
     plt.xlabel('Time (seconds)')
     plt.ylabel('Amplitude')
     plt.tight_layout()
-    plt.show()
+    plt.savefig(save_path, facecolor='#1F1B24')
+    plt.close()
 
 
-def dis_mel(y, sr):
+def dis_mel(y, sr, save_path):
     # plot mel filter
     # Create the Mel filter bank
     n_fft = 2048  # Number of FFT components
@@ -80,7 +83,8 @@ def dis_mel(y, sr):
     plt.ylabel('Mel Filter Index')
     plt.colorbar(img, format="%+2.0f dB") 
     plt.tight_layout()
-    plt.show()
+    plt.savefig(save_path, facecolor='#1F1B24')
+    plt.close()
 
 
 def load_audio(file_path):
@@ -90,19 +94,43 @@ def load_audio(file_path):
     duration = librosa.get_duration(y=y, sr=sr)
     return y, sr, duration
 
-def main():
-    audio_file = 'TestAudioFiles/phub_and.mp3'
-    y, sr, duration = load_audio(audio_file)
+# takes arguments from GUI-graphs.py and returns location of generated graph
+def make_graph(file_path, graph_type, output_folder="OutputGraphs"):
+    y, sr, duration = load_audio(file_path)
+    # Create filename and path to store generated graph
+    output_path = os.path.join(output_folder, f"{graph_type}_graph.png")
 
-    # Some file info
-    print(f"File: {audio_file}")
-    print(f"Sample rate: {sr} Hz")
-    print(f"Total samples: {len(y)}")
-    print(f"Duration: {duration:.2f} s")
+    # Call selected graph function
+    if graph_type == "MFCC":
+        dis_mfccs(y, sr, output_path)
+    elif graph_type == "Spectrogram":
+        dis_spectrogram(y, sr, output_path)
+    elif graph_type == "Beat Markers":
+        dis_beatmarkers(y, sr, output_path)
+    elif graph_type == "Waveform":
+        dis_waveform(y, sr, output_path)
+    elif graph_type == "Mel":
+        dis_mel(y, sr, output_path)
+    else:
+        raise ValueError(f"Unsupported graph type: {graph_type}")
+    return output_path
+        
 
-    # Call functions here
-    dis_mfccs(y, sr)
+#### Keep commented when using with GUI-graphs.py, will throw RuntimeError: 
+#### Please destroy the QApplication singleton before creating a new QApplication instance
+# def main():
+#     audio_file = 'TestAudioFiles/phub_and.mp3'
+#     y, sr, duration = load_audio(audio_file)
+
+#     # Some file info
+#     print(f"File: {audio_file}")
+#     print(f"Sample rate: {sr} Hz")
+#     print(f"Total samples: {len(y)}")
+#     print(f"Duration: {duration:.2f} s")
+
+#     # Call functions here
+#     dis_mfccs(y, sr)
 
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
